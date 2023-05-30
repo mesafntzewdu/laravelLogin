7163200dc4dd232a0440ad05a7c19e6dbf8a39e4
mesafnt <mesafntzewdu@gmail.com>
2023-05-30 11:56:36 +0300

    updating login and signup view

---
+++ resources/views/forget.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!---Coding By CoderGirl | www.codinglabweb.com--->
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Rest password</title>
+  <!---Custom CSS File--->
+  <link rel="stylesheet" href="{{ asset('asset/css/style.css')}}">
+</head>
+<body>
+  <div class="container">
+    <input type="checkbox" id="check">
+    <div class="login form">
+      <header>Reset password</header>
+
+      <form action="" method="post">
+        <input type="email" name="email" placeholder="Enter your email">
+        <input type="button" class="button" value="Get" >
+        <a href="/">I got my password</a>
+      </form>
+
+      </div>
+    </div>
+  </div>
+</body>
+</html>
